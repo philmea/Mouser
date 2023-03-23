1064659a989ef602b468ab3b63769d6c75cc2e85
Phil Meadows <philmea@microsoft.com>
2023-03-23 16:58:09 -0700

    Adding mouser draft ps.1

---
+++ mouser.ps1
@@ -0,0 +1,65 @@
+
+function Get-Cursor {
+    [CmdletBinding()]
+    Param (   
+    )
+    
+    Process{
+        Add-Type -AssemblyName System.Windows.Forms
+        # https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.cursor?view=windowsdesktop-7.0
+        # https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.cursor.position?view=windowsdesktop-7.0
+        $x = [System.Windows.Forms.Cursor]::Position.X
+        $y = [System.Windows.Forms.Cursor]::Position.Y
+        Write-Output @{"x"=$x;"y"=$y}
+    }
+}
+
+Function Set-Cursor {
+    [CmdletBinding()]
+    Param (
+            [int]$x = 0, [int]$y = 0
+    
+        )
+    
+    Process {
+    
+            Add-Type -AssemblyName System.Windows.Forms
+            # https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.systeminformation?view=windowsdesktop-7.0
+            # https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.systeminformation.virtualscreen?view=windowsdesktop-7.0
+            <#
+            The VirtualScreen property indicates the bounds of the entire desktop on a multi-monitor system. You can use this property to determine the maximum visual space available on a system that has multiple monitors installed.
+            #>
+            $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+            if ($x -gt $screen.Width){
+                Write-Error -Message "Width coordinate 'x' is greater than the width of your screen."
+            }
+            elseif ($y -gt $screen.Height) {
+                Write-Error -Message "Height coordinate 'y' is greater than the height of your screen."
+            }
+            else{
+                # https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.cursor.position?view=windowsdesktop-7.0
+                <#Gets or sets the cursor's position.
+                #>
+                [System.Windows.Forms.Cursor]::Position = "$($x),$($y)"
+                Write-Output "Cursor moved to x: $x, y: $y."
+            }
+        }
+    }
+
+function Send-Click{
+    [CmdletBinding()]
+    param (
+        [int]$clicks = 1
+    )
+Process{
+    Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32SendClick -Namespace W;
+    # https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-mouse_event
+    $click = 1
+    while ($click -le $clicks){
+        [W.U32SendClick]::mouse_event(0x00000002, 0, 0, 0, 0);
+        [W.U32SendClick]::mouse_event(0x00000004, 0, 0, 0, 0);
+        Start-Sleep -Milliseconds 50
+        $click++
+        }
+    }
+}
