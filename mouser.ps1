16c42d5a8de98ba415b2a7d06c38805f5ab5b3e8
Phil Meadows <philmea@microsoft.com>
2023-03-24 17:13:58 -0700

    Mouse movement behavior for Send-Click updated

--- mouser.ps1
+++ mouser.ps1
@@ -17,7 +17,11 @@ function Get-Cursor {
 Function Set-Cursor {
     [CmdletBinding()]
     Param (
-            [int]$x = 0, [int]$y = 0
+            [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+            [int]$x = 0, 
+            [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+            [int]$y = 0,
+            [hashtable]$posHash
     
         )
     
@@ -49,13 +53,23 @@ Function Set-Cursor {
 function Send-Click{
     [CmdletBinding()]
     param (
-        [int]$clicks = 1
+        [int]$clicks = 1,
+        $stationary = $false
     )
 Process{
     Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32SendClick -Namespace W;
     # https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-mouse_event
     $click = 1
+    if ($stationary = $true){
+        $pos = Get-Cursor
+    }
     while ($click -le $clicks){
+        if ($stationary = $true){
+            $currentPos = Get-Cursor
+            if($currentPos.x -ne $pos.x -and $currentPos.y -ne $pos.y){
+                break
+            }
+        }
         [W.U32SendClick]::mouse_event(0x00000002, 0, 0, 0, 0);
         [W.U32SendClick]::mouse_event(0x00000004, 0, 0, 0, 0);
         Start-Sleep -Milliseconds 50
