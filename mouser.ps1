3aad158706d63ee606df3d4bc3fb1709f54ea98e
Phil Meadows <philmea@microsoft.com>
2023-03-26 10:38:21 -0700

    stationary fixed

--- mouser.ps1
+++ mouser.ps1
@@ -60,16 +60,16 @@ Process{
     Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32SendClick -Namespace W;
     # https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-mouse_event
     $click = 1
-    if ($stationary = $true){
-        $pos = Get-Cursor
-    }
+    $pos = Get-Cursor
+
     while ($click -le $clicks){
-        if ($stationary = $true){
+        if ($stationary -eq $true){
             $currentPos = Get-Cursor
             if($currentPos.x -ne $pos.x -and $currentPos.y -ne $pos.y){
                 break
             }
         }
+
         [W.U32SendClick]::mouse_event(0x00000002, 0, 0, 0, 0);
         [W.U32SendClick]::mouse_event(0x00000004, 0, 0, 0, 0);
         Start-Sleep -Milliseconds 50
@@ -77,3 +77,4 @@ Process{
         }
     }
 }
+
